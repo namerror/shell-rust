994c643ca2cbd37d87a6113400e283cfb1453c37
Leon <leonlong2004@outlook.com>
2026-07-24 13:45:43 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use std::{io::Read, print};
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
-    // io::stdout().flush().unwrap();
+    io::stdout().flush().unwrap(); // uses flush to ensure the prompt is displayed before reading input
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+    println!("{}: command not found", command.trim());
+
 }
