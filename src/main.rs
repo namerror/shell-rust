737edb29cbcf69730a0b52aa96e8064af4ae983f
Leon <leonlong2004@outlook.com>
2026-07-24 14:01:34 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,11 @@ fn main() {
         io::stdout().flush().unwrap(); // uses flush to ensure the prompt is displayed before reading input
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
+
+        if command.trim() == "exit" {
+            break;
+        }
+
         println!("{}: command not found", command.trim());        
     }
 
