15551e2f7a28c143ff6963d5a6ee4087c88d39e9
Leon <leonlong2004@outlook.com>
2026-07-24 14:50:27 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 use std::io::{self, Write};
 use std::{io::Read, print};
 
+
+
 fn main() {
     loop {
         print!("$ ");
@@ -10,13 +12,18 @@ fn main() {
         io::stdin().read_line(&mut command).unwrap();
 
         let command = command.trim();
+        let builtins = ["exit", "echo", "type"];
 
         if command == "exit" {
             break;
-        }
-
-        if command.starts_with("echo") {
+        } else if command.starts_with("echo") {
             println!("{}", &command[5..]);
+        } else if command.starts_with("type") {
+            if builtins.contains(&&command[5..]) {
+                println!("{} is a shell builtin", &&command[5..]);
+            } else {
+                println!("{}: command not found", &&command[5..]);
+            }
         } else {
             println!("{}: command not found", command);
         }
