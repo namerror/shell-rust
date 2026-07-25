ee292c3e7e92181bf59a5429b9b460c76458167d
Leon <leonlong2004@outlook.com>
2026-07-25 14:29:22 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+use core::error;
+use std::fs::metadata;
+use std::io::Error;
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::{io::Read, print};
-
+use std::os::unix::fs::PermissionsExt;
+use std::{print};
+use std::{env, fs};
 
 
 fn main() {
     loop {
+        let path = env::var("PATH").unwrap();
+        let paths: Vec<&str> = path.split(":").collect();
         print!("$ ");
         io::stdout().flush().unwrap(); // uses flush to ensure the prompt is displayed before reading input
         let mut command = String::new();
@@ -14,18 +20,43 @@ fn main() {
         let command = command.trim();
         let builtins = ["exit", "echo", "type"];
 
+        let args: Vec<&str> = command.split(' ').collect();
+
         if command == "exit" {
             break;
         } else if command.starts_with("echo") {
             println!("{}", &command[5..]);
         } else if command.starts_with("type") {
-            if builtins.contains(&&command[5..]) {
-                println!("{} is a shell builtin", &&command[5..]);
+
+            if builtins.contains(&args[1]) {
+                println!("{} is a shell builtin", &args[1]);
             } else {
-                println!("{}: not found", &&command[5..]);
+                match find_executable(paths, &args[1]) {
+                    Ok(v) => println!("{} is {}", &args[1], v),
+                    Err(_error) => println!("{}: not found", &args[1]),
+                };
             }
         } else {
             println!("{}: command not found", command);
         }
     }
+}
+
+fn find_executable(paths: Vec<&str>, command: &str) -> Result<String, Error> {
+    for path in paths {
+        for entry in fs::read_dir(path)? {
+            let path = entry?.path();
+            if path.is_file() && path.file_name().is_some_and(|x| x==command) {
+                let metadata = metadata(&path)?;
+                if (metadata.permissions().mode() & 0o111) != 0 {
+                    return match path.into_os_string().into_string() {
+                        Ok(s) => Ok(s),
+                        Err(_s) => Err(Error::new(io::ErrorKind::Other, "Failed"))
+                    }
+                }
+            }
+        }
+    }
+
+    Err(Error::new(io::ErrorKind::Other, "Failed"))
 }
